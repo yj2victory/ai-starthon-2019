a770d32799a23ebdbeeb3685485cc663696c493f
Yong-Ju Lee <64128895+yj2victory@users.noreply.github.com>
2020-05-10 12:10:33 +0900

    Update data_loader.py

--- det10+11+12+13/data_loader.py
+++ det10+11+12+13/data_loader.py
@@ -2,7 +2,6 @@
 Replicated in the NSML leaderboard dataset, KoreanFoodLocalize.
 """
 
-from nsml import IS_ON_NSML, DATASET_PATH
 
 import os
 
@@ -13,10 +12,7 @@ def feed_infer(output_file, infer_func):
     output_file(str): inference 후 결과값을 저장할 파일의 위치 패스
      (이위치에 결과를 저장해야 evaluation.py 에 올바른 인자로 들어옵니다.)
     """
-    if IS_ON_NSML:
-        root_path = os.path.join(DATASET_PATH, 'test')
-    else:
-        root_path = '/home/data/NIPAKoreanFoodLocalizeSmall/test'
+    root_path = '/content/gdrive/My Drive/Colab/11_idet5_car'
     results = infer_func(root_path)
     results_str = [','.join(str(v) for v in l) for l in list(results)]
     print('write output')
